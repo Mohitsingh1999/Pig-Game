e0faa13f8200a91f81ee0a4c4a7749e356efd425
mohit <ms0135048@gmail.com>
2024-10-20 18:32:38 +0530

    completed dice roll functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const player0El=document.querySelector('.player--0');
+const player1El=document.querySelector('.player--1');
+
+
+
 const score0El =document.getElementById('score--0');
 const score1El=document.getElementById('score--1');
 
@@ -15,7 +20,9 @@ score0El.textContent=0;
 score1El.textContent=0;
 diceEl.classList.add('hidden');
 
-let currentScore=0;
+let currentScore = 0;
+let activePlayer=0;
+const scores=[0,0];
 
 //Rolling dice functionalty
 btnRoll.addEventListener('click',function(){
@@ -23,20 +30,27 @@ btnRoll.addEventListener('click',function(){
     const dice = Math.trunc(Math.random() * 6) + 1;
    // console.log(dice);
     //display dice
-    diceEl.classList.remove('hidden');
+ 
     diceEl.src= `img/dice-${dice}.png`;
+   
+    diceEl.classList.remove('hidden');
 
     //check for rolled 1
-    if(dice!=1){
+    if(dice !== 1){
         currentScore+=dice;
-        current0El.textContent=currentScore;
+        document.getElementById(`current--${activePlayer}`).textContent=currentScore;
+
+      //  current0El.textContent=currentScore;
 
     }else{
+
         //Switching the player if dice is 1
-        
+        document.getElementById(`current--${activePlayer}`).textContent=0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore=0;   
+        player0El.classList.toggle('player--active');
+        player1El.classList.toggle('player--active');
     }
-
-
 });
     
 
